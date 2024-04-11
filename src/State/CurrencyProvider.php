20e6cce43db87aeda37ac548555b635a474f70ae
MmichalMmajewski <mmajewski.mmichal@gmail.com>
2024-04-12 00:59:28 +0200

    FEAT Add inherit to CurrencyProvider form AbstractController and pass variables from env, instead of static strings

--- src/State/CurrencyProvider.php
+++ src/State/CurrencyProvider.php
@@ -8,21 +8,23 @@ use App\Entity\Currency;
 use App\Service\CurrencyService;
 use App\Service\ExternalApiConnectionService;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class CurrencyProvider implements ProviderInterface
+class CurrencyProvider extends AbstractController implements ProviderInterface
 {
     public function __construct(
         private readonly ExternalApiConnectionService $externalApiConnectionService,
         private readonly CurrencyService $currencyService,
-        private readonly EntityManagerInterface $entityManager
+        private readonly EntityManagerInterface $entityManager,
     ){
     }
 
     public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
     {
+        $wholeEndpoint = $this->getParameter('app.nbp_exchange_rates_endpoint').$this->getParameter('app.nbp_exchange_rates_table_one');
         $response = $this->externalApiConnectionService->getDecodeResponseByUriAndEndpoint(
-            'https://api.nbp.pl/',
-            'api/exchangerates/tables/a'
+            $this->getParameter('app.nbp_api_url'),
+            $wholeEndpoint
         );
 
         //Return catch error with HTTP code response
